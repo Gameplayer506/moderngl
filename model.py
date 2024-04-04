5bc2ce39828fb1e4c698f82988a870ea5e853f67
isadot0 <506smoothie@gmail.com>
2024-04-04 12:45:02 +0100

    fixed indentation error with BaseModel class (all functions were in __init__)

--- model.py
+++ model.py
@@ -12,15 +12,15 @@ class BaseModel:
         self.program = self.vao.program
         self.camera = self.app.camera
 
-        def get_model_matrix(self):
-            m_model = glm.mat4()
-            return m_model
-        
-        def render(self):
-            self.update
-            self.vao.render()
+    def get_model_matrix(self):
+        m_model = glm.mat4()
+        return m_model
+    
+    def render(self):
+        self.update
+        self.vao.render()
 
-        def update(self): ...
+    def update(self): ...
 
 class Cube(BaseModel):
     def __init__(self, app, vao_name="cube", tex_id=0):
@@ -30,21 +30,21 @@ class Cube(BaseModel):
     def update(self):
         self.texture.use()
         #wrtie in changes for shader
-        self.shader_program["m_model"].write(self.m_model)
-        self.shader_program['m_view'].write(self.app.camera.m_view)
-        self.shader_program["camPos"].write(self.app.camera.position)
+        self.program["m_model"].write(self.m_model)
+        self.program['m_view'].write(self.app.camera.m_view)
+        self.program["camPos"].write(self.app.camera.position)
 
     def on_init(self):
         #light
-        self.shader_program["light.position"].write(self.app.light.position)
-        self.shader_program["light.Ia"].write(self.app.light.Ia)
-        self.shader_program["light.Id"].write(self.app.light.Id)
-        self.shader_program["light.Is"].write(self.app.light.Is)
+        self.program["light.position"].write(self.app.light.position)
+        self.program["light.Ia"].write(self.app.light.Ia)
+        self.program["light.Id"].write(self.app.light.Id)
+        self.program["light.Is"].write(self.app.light.Is)
         #texture
         self.texture = self.app.mesh.texture.textures[self.tex_id]
         self.program["u_texture_0"] = 0
         self.texture.use()
         #mvp
-        self.shader_program['m_proj'].write(self.app.camera.m_proj)
-        self.shader_program['m_view'].write(self.app.camera.m_view)
-        self.shader_program['m_model'].write(self.m_model)
+        self.program['m_proj'].write(self.app.camera.m_proj)
+        self.program['m_view'].write(self.app.camera.m_view)
+        self.program['m_model'].write(self.m_model)
